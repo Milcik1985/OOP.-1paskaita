c15be21d460b937bb82a9437d82de975cd9b7316
Milcik1985 <johndoe@example.commilcik@gmail.com>
2024-02-29 10:18:39 +0200

    Task

---
+++ src/script.js
@@ -0,0 +1,10 @@
+// 1. Sukurti User klasę;
+// 2. Klasėje turi būt reikšmės: name, age, nickName, password, dateCreated bei id;
+// 3. name, age, nickName, password turi būt paduodami kuriant naują instance. (per konstruktorių);
+// 4. dataCreated bei id turi būt sugeneruojami automatiškai. (reiks parašyt metodus kurie sugeneruoja reikšmes);
+// 5. name ir age turi turėt getterį bei setterį, nickName tik getterį;
+// 6. Visi kintamieji turi būt private;
+// 7. Visi metodai naudojami tik klasės viduje turi būt private;
+// 8. Sukurti metodą, kuris atvaizduoja userio info išskyrus password;
+// 9. Sukurti UserCreationType bei priskirti konstruktoriui;
+// 10. Sukurti naują user index.ts faile. panaudoti getterius, setterius. Pabandyt displayint userį.
